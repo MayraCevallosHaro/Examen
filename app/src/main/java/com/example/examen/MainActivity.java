0c3ba65434e916b3808b1e0a8cbee06f204fa134
MayraCevallosHaro <57886405+MayraCevallosHaro@users.noreply.github.com>
2021-03-11 18:28:33 -0500

    ....

--- app/src/main/java/com/example/examen/MainActivity.java
+++ app/src/main/java/com/example/examen/MainActivity.java
@@ -12,24 +12,33 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements Asynchtask {
 
+    List<Revistas> revistas = new ArrayList<Revistas>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Map<String, String> data = new HashMap<String, String>();
-        WebService ws= new WebService("https://revistas.uteq.edu.ec/ws/journals.php", data, this, this);
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService("https://revistas.uteq.edu.ec/ws/journals.php", datos, this, this);
         ws.execute("");
     }
 
     @Override
     public void processFinish(String result) throws JSONException {
-        jsonObject =  new JSONObject(result);
-        jsonObject = jsonObject.getJSONObject("Results");
-        try {
-            String m = "";
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
 
+        JSONArray jArray = new JSONArray(result);
+        for(int i=0; i<jArray.length(); i++){
+            JSONObject json_data = jArray.getJSONObject(i);
+            Revistas revista = new Revistas(
+                    json_data.getString("journal_id"),
+                    json_data.getString("portada"),
+                    json_data.getString("abbreviation"),
+                    json_data.getString("description"),
+                    json_data.getString("journalThumbnail"),
+                    json_data.getString("name")
+            );
+            revistas.add(revista);
+        }
+        String a = "";
     }
 }
