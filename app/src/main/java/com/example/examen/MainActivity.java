1bc279ec16122c6ceb190103238c5abccee024cb
MayraCevallosHaro <57886405+MayraCevallosHaro@users.noreply.github.com>
2021-03-11 17:48:41 -0500

    ...

--- app/src/main/java/com/example/examen/MainActivity.java
+++ app/src/main/java/com/example/examen/MainActivity.java
@@ -3,23 +3,19 @@ package com.example.examen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import com.example.examen.WebServices.Asynchtask;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Asynchtask {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    }
-
-    public void cargar(){
         Map<String, String> data = new HashMap<String, String>();
         WebService ws= new WebService("https://revistas.uteq.edu.ec/ws/journals.php", data, this, this);
         ws.execute("");
