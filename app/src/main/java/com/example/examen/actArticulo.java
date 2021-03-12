4812a38ece41a1538054d9e28af1c62a34a867be
MayraCevallosHaro <57886405+MayraCevallosHaro@users.noreply.github.com>
2021-03-12 00:26:44 -0500

    12

--- app/src/main/java/com/example/examen/actArticulo.java
+++ app/src/main/java/com/example/examen/actArticulo.java
@@ -1,9 +1,12 @@
 package com.example.examen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
+import com.example.examen.Adapters.AdaptadorArticulos;
+import com.example.examen.Adapters.AdaptadorEdiciones;
 import com.example.examen.Models.Articulos;
 import com.example.examen.Models.Ediciones;
 import com.example.examen.WebServices.Asynchtask;
@@ -11,7 +14,9 @@ import com.example.examen.WebServices.WebService;
 import com.example.examen.ui.main.ActArticuloFragment;
 import com.mindorks.placeholderview.PlaceHolderView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,7 +24,7 @@ import java.util.List;
 import java.util.Map;
 
 public class actArticulo extends AppCompatActivity implements Asynchtask {
-    PlaceHolderView phvEdiciones;
+    PlaceHolderView phvArticulos;
     List<Articulos> articulos = new ArrayList<Articulos>();
 
 
@@ -35,18 +40,36 @@ public class actArticulo extends AppCompatActivity implements Asynchtask {
 
         Map<String, String> datos = new HashMap<String, String>();
 
-        phvEdiciones = (PlaceHolderView) findViewById(R.id.phv);
+        phvArticulos = (PlaceHolderView) findViewById(R.id.phvArticulos);
 
         //rclRevistas = findViewById(R.id.rclRevistas);
         WebService ws= new WebService("https://revistas.uteq.edu.ec/ws/journals.php", datos, this, this);
         ws.execute("");
-
-
     }
 
 
     @Override
     public void processFinish(String result) throws JSONException {
+        phvArticulos.getBuilder()
+                .setHasFixedSize(false)
+                .setItemViewCacheSize(10)
+                .setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
 
+        JSONArray jArray = new JSONArray(result);
+        for(int i=0; i<jArray.length(); i++){
+            JSONObject json_data = jArray.getJSONObject(i);
+            Articulos.Galeys gals = (Articulos.Galeys) json_data.get("galeys");
+            Articulos.Authors auth = (Articulos.Authors) json_data.get("authors");
+            Articulos articulo = new Articulos(
+                    json_data.getString("section"),
+                    json_data.getString("publication_id"),
+                    json_data.getString("title"),
+                    json_data.getString("date_published"),
+                    gals, auth
+            );
+            articulos.add(articulo);
+            phvArticulos.addView(new AdaptadorArticulos(articulo,getApplicationContext(), phvArticulos));
+        
     }
+}
 }
